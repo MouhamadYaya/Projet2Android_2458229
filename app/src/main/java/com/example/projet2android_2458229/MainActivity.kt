092abbaedd1ab9cefe6476837c153a1384e34e9d
Mouhamad Yaya <mouhamadyaya@yahoo.com>
2025-11-04 21:50:55 -0500

    navigation test

--- app/src/main/java/com/example/projet2android_2458229/MainActivity.kt
+++ app/src/main/java/com/example/projet2android_2458229/MainActivity.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.projet2android_2458229.ui.theme.Projet2Android_2458229Theme
 
 sealed class Screen(val route: String) {
@@ -47,8 +55,38 @@ fun SettingsScreen() {
 }
 
 // Navigation test
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppNavigation() {}
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { TopAppBar(title = { Text("Projet Android") }) }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) {
+                HomeScreen()
+            }
+
+            composable(
+                Screen.Detail.route,
+                arguments = listOf(navArgument("id") { type = NavType.IntType })
+            ) {
+                val id = it.arguments?.getInt("id") ?: 0
+                DetailScreen(id)
+            }
+
+            composable(Screen.Settings.route) {
+                SettingsScreen()
+            }
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
